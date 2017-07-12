7127ddbe86b9b6935454dd1e45cf2324fda05eb2
Obscurious <regs.noreply@ya.ru>
2017-07-12 17:46:31 +0300

    Create dummy model

--- migrate/20170712144205_create_accounts.rb
+++ migrate/20170712144205_create_accounts.rb
@@ -1,7 +1,8 @@
 Sequel.migration do
   change do
-    create_table(:model1s) do
+    create_table(:account) do
       primary_key :id
+      string      :name, unique: true
     end
   end
 end
--- spec/model/account_spec.rb
+++ spec/model/account_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-describe Model1 do
+describe Account do
   it "should " do
     # ...
   end
