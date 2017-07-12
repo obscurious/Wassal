3bebef68cd48075b7bde6521b10826a19e12d7af
Obscurious <regs.noreply@ya.ru>
2017-07-12 17:19:57 +0300

    remove unused

--- Gemfile
+++ Gemfile
@@ -1,13 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rack_csrf'
-gem 'tilt', '>= 2'
-gem 'erubi'
-gem 'roda'
 gem 'sequel'
 gem 'sequel_pg'
 gem 'rack-unreloader'
 
-gem 'capybara'
 gem 'minitest', '>= 5.7.0'
 gem 'minitest-hooks', '>= 1.1.0'
--- spec/web/spec_helper.rb
+++ spec/web/spec_helper.rb
@@ -2,15 +2,10 @@ ENV["RACK_ENV"] = "test"
 require_relative '../../app'
 raise "test database doesn't end with test" unless DB.opts[:database] =~ /test\z/
 
-require 'capybara'
-require 'capybara/dsl'
 require 'rack/test'
 
 require_relative '../minitest_helper'
 
-Capybara.app = Wassal.freeze
-
 class Minitest::Spec
   include Rack::Test::Methods
-  include Capybara::DSL
 end
