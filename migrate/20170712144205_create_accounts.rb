2768e5e8628e83586e16034904f7f8486ecb6c35
Obscurious <regs.noreply@ya.ru>
2017-07-12 17:52:53 +0300

    Gemfile

--- migrate/20170712144205_create_accounts.rb
+++ migrate/20170712144205_create_accounts.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table(:account) do
       primary_key :id
-      string      :name, unique: true
+      text        :name, unique: true
     end
   end
 end
